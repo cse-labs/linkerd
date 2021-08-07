26899399050420e50e30066b01ec48d663973a8d
Bart Robertson <bartr@microsoft.com>
2021-08-07 01:51:13 +0000

    updated dev container

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -7,4 +7,9 @@ sudo rm -f /grafana/grafana.db
 sudo cp deploy/grafanadata/grafana.db /grafana
 sudo chown -R 472:472 /grafana
 
+docker network create kind
+
+# create local registry
+docker run -d --net kind --restart=always -p "127.0.0.1:5000:5000" --name kind-registry registry:2
+
 echo "on-create complete" >> ~/status
