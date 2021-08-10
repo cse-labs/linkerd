43f25e6c7b26826c38fbe0f9220f5d918cb772e4
bartr <bartr@microsoft.com>
2021-08-10 07:22:59 +0000

    updated dev container

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -12,4 +12,11 @@ docker network create kind
 # create local registry
 docker run -d --net kind --restart=always -p "127.0.0.1:5000:5000" --name kind-registry registry:2
 
+# download rio
+curl -sfL https://get.rio.io | sh -
+
+# install rust musl target
+sudo apt-get install -y --no-install-recommends musl-tools
+rustup target add x86_64-unknown-linux-musl
+
 echo "on-create complete" >> ~/status
