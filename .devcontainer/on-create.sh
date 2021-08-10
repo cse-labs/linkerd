0441308d78b1ca313696b5ada1c2c6cddbd98cbc
bartr <bartr@microsoft.com>
2021-08-10 19:29:23 +0000

    updated dev container

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -10,10 +10,18 @@ sudo chown -R 472:472 /grafana
 docker network create kind
 
 # create local registry
-#docker run -d --net kind --restart=always -p "127.0.0.1:5000:5000" --name kind-registry registry:2
+#docker run -d --net kind --restart=always -p "127.0.0.1:5000:5000" --name registry registry:2
+k3d registry create registry.localhost --port 5000
+docker network connect kind k3d-registry.localhost
+
+# push ngsa-app to local repo
+docker pull ghcr.io/retaildevcrews/ngsa-app:beta
+docker tag ghcr.io/retaildevcrews/ngsa-app:beta k3d-registry.localhost:5000/ngsa:local
+docker push k3d-registry.localhost:5000/ngsa:local
+docker rmi ghcr.io/retaildevcrews/ngsa-app:beta
 
 # download rio
-curl -sfL https://get.rio.io | sh -
+#curl -sfL https://get.rio.io | sh -
 
 # install rust musl target
 sudo apt-get install -y --no-install-recommends musl-tools
