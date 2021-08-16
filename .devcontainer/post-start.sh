09cc3aa0908a9d964cc9058f19b5372917a5fd28
Dan Massey <danmass@microsoft.com>
2021-08-16 19:28:02 +0000

    removing extra echo

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -3,6 +3,6 @@
 echo "post-start start" >> ~/status
 
 rm -f ../app/depb/keys/pickle.key
-echo echo $PICKLE_PRIVATE_KEY >> ../app/depb/keys/pickle.key
+echo $PICKLE_PRIVATE_KEY >> ../app/depb/keys/pickle.key
 
 echo "post-start complete" >> ~/status
