33fa63a5570053b3893d5fce26c9a3b1fc4e46d1
Dan Massey <danmass@microsoft.com>
2021-08-27 18:41:30 +0000

    signing with ring, instead of openssl

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -2,7 +2,7 @@
 
 echo "post-start start" >> ~/status
 
-rm -f /workspaces/linkerd/app/rust/signer/keys/pickle.key
-echo "$PICKLE_PRIVATE_KEY" >> /workspaces/linkerd/app/rust/signer/keys/pickle.key
+rm -f /workspaces/linkerd/app/rust/signer/keys/pickle_key.der
+echo "$PICKLE_PRIVATE_KEY" | openssl base64 -d >> /workspaces/linkerd/app/rust/signer/keys/pickle_key.der
 
 echo "post-start complete" >> ~/status
