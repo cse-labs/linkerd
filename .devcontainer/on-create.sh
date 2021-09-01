b6b1a4423ad5713482ab74cbf72aa8bea8f3b94d
Bart Robertson <bartr@microsoft.com>
2021-09-01 02:52:40 -0500

    replace kind with k3d

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -7,11 +7,10 @@ sudo rm -f /grafana/grafana.db
 sudo cp deploy/grafanadata/grafana.db /grafana
 sudo chown -R 472:0 /grafana
 
-docker network create kind
+docker network create k3d
 
 # create local registry
-#docker run -d --net kind --restart=always -p "127.0.0.1:5000:5000" --name registry registry:2
 k3d registry create registry.localhost --port 5000
-docker network connect kind k3d-registry.localhost
+docker network connect k3d k3d-registry.localhost
 
 echo "on-create complete" >> ~/status
