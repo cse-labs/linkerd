878f94f28ebc60209aa5c09e4119b69c1c4afb48
Bart Robertson <bartr@microsoft.com>
2021-09-20 07:37:05 -0500

    added comment (#37)

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -2,6 +2,7 @@
 
 echo "post-start start" >> ~/status
 
+# Store the private key in GitHub Secrets to a file
 rm -f /workspaces/linkerd/app/rust/signer/keys/pickle_key.der
 echo "$PICKLE_PRIVATE_KEY" | openssl base64 -d >> /workspaces/linkerd/app/rust/signer/keys/pickle_key.der
 
